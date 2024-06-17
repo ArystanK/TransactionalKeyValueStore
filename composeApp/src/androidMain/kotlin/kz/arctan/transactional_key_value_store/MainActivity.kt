fc517d3d946cc74fbffb97256b422e044a0dccef
Arctan <aarystan@outlook.com>
2024-06-17 18:43:49 +0500

    Unnecessary preview is removed

--- composeApp/src/androidMain/kotlin/kz/arctan/transactional_key_value_store/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/kz/arctan/transactional_key_value_store/MainActivity.kt
@@ -1,11 +1,9 @@
 package kz.arctan.transactional_key_value_store
 
-import kz.arctan.transactional_key_value_store.presentation.App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import kz.arctan.transactional_key_value_store.presentation.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +13,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
